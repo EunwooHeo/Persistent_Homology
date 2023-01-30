39913f3bd0c2ca665a1f31007a63c4e725713eb2
EunwooHeo <93895678+EunwooHeo@users.noreply.github.com>
2023-01-30 23:43:58 +0900

    Update Vietoris_Rips_Complex.py
    Improve the all code and make the 2-dim homology code

--- Vietoris_Rips_Complex.py
+++ Vietoris_Rips_Complex.py
@@ -39,39 +39,6 @@ class convert_uppertriangle:
             self._matrix[i]=tmp
         self._uppermatrix=self._matrix
 
-
-class sort_nparray_index_alongvalue:
-    """
-    numpy array를 입력받아 낮은 값부터 차례로 그 index의 정보를 나타낸다. (값,(인덱스 정보))순으로
-    """
-    def __init__(self):
-        pass     
-    
-    @property
-    def array(self):
-        return self._array
-    
-    @array.setter
-    def array(self,array):
-        if not isinstance(array,np.ndarray):
-            raise Exception("The type must be numpy.ndarray")
-        else:
-            self._array=array
-    
-    def sort(self):
-        p=[]
-        arr=self.array
-        for i in arr:
-            for j in i:
-                p.append(j)
-        p.sort()
-        q=list(set(p)) 
-        q.sort()
-        array_info=[]
-        l=len(q)
-        for i in range(1,l):
-            array_info.append((q[i],np.where(self.array==q[i])))
-        self.array_info=array_info   
         
 def vec_operation_mod_p(v,w,mod): #a를 b에다 더함 주의 (방향성 있음)
     n=mod
@@ -157,114 +124,110 @@ def Caculate_Vietoris_Ripscomplex_Standardalgorithm(distmat,max_homology_dim):
     if max_dim < 1 or not isinstance(max_dim,int):
         raise Exception("max_dim must be at least 1 or int type")
 
-    M=convert_uppertriangle()
-    M.matrix=dist_mat
-    M.convert()
-    dist_mat=M._uppermatrix    
 
     for i in range(max_dim+1):
         globals()['S_{}'.format(i)]=[]
-        globals()['S_{}_b'.format(i)]=[]
 
     l_1=len(dist_mat)
     for i in range(l_1):
         S_0.append((i,))
-        S_0_b.append(((i,),0))
 
-    a=sort_nparray_index_alongvalue()
-    a.array=dist_mat
-    a.sort()
-    S_1_info=a.array_info
 
-    matrix_index=S_0_b.copy() 
-    matrix_index_novalue=S_0.copy()
+    S_1_info=[]
+    for i in range(len(dist_mat)):
+        for j in range(i,len(dist_mat)):
+            if not dist_mat[i][j]==0:
+                S_1_info.append([dist_mat[i][j],i,j])
+    S_1_info.sort()
 
     l_2=len(S_0)
     matrix_index_dict={(i,):i for i in range (l_2)}
     matrix_col_nonzero={i:() for i in range (l_2)} 
     simplex_value={(i,):0 for i in range (l_2)}
 
+
     col_loca=l_2
 
-#     adjacent_matrix={}
-#     for i in range(len(S_0)):
-#         adjacent_matrix[i]=[0 for k in range(len(S_0))]
-        
-      
     adjacent_matrix=defaultdict(dict)
     for i in range(len(S_0)):
-        for j in range(len(S_0)):
-            adjacent_matrix[i][j]=0
+        tmp=defaultdict(dict)
+        adjacent_matrix[i]=tmp
+
 
     for info in S_1_info:
         birth=info[0] 
-        l_3=len(info[1][0]) 
-        for j in range(l_3):
-            edge=(info[1][0][j],info[1][1][j]) 
-            if edge not in S_1:
+        row_loca=edge=(info[1],info[2]) # =row_loca
+        new=(edge,birth) 
 
-                new=(edge,birth) 
+        birth_list=[(new,row_loca)] 
 
-                birth_list=[(new,edge)] 
+        while 1:
 
-                while 1:
+            if len(birth_list)!=0:
 
-                    if len(birth_list)!=0:
+                birth_object=birth_list[0][0] #new=(edge,birth)
+                A=birth_object[0] 
+                B=birth_object[1]            
 
-                        birth_object=birth_list[0][0] 
-                        l_4=len(birth_object[0])
-                        A=birth_object[0] 
-                        B=birth_object[1]
-                        globals()[f'S_{l_4-1}'].append(tuple(A)) 
-                        globals()[f'S_{l_4-1}_b'].append((tuple(A),B))
-                        simplex_value[tuple(A)]=B
-                        L=0
-                        for d in range(max_dim+1):
-                            L+=len(globals()['S_{}'.format(d)])
-                        matrix_index_dict[A]=L-1
-                        matrix_index.append(birth_object)
-                        matrix_index_novalue.append(A)
-                        matrix_col_nonzero[col_loca]=birth_list[0][1] 
+                l_4=len(A)
+                globals()[f'S_{l_4-1}'].append(tuple(A)) #S_1 에 edge 넣고, S_2에 face 넣고, ...
 
-                        col_loca+=1
-                        birth_list.remove(birth_list[0])
+                simplex_value[tuple(A)]=B
+                L=0
+                for d in range(max_dim+1):
+                    L+=len(globals()['S_{}'.format(d)])
+                matrix_index_dict[A]=L-1 #이부분을 col_loca로 안하는 이유는?
+                matrix_col_nonzero[col_loca]=birth_list[0][1] 
 
-                        if l_4==2:
-                            a,b=A
-                            adjacent_matrix[a][b]=1
-                            adjacent_matrix[b][a]=1
 
-                        if max_dim >= l_4: 
+                col_loca+=1
+                birth_list.remove(birth_list[0])    
 
-                            test=globals()[f'S_{l_4-1}']  
+                adjacent_check_member=[]
+                for i in range(l_4):
+                    tmp=list(A)
+                    tmp.remove(A[i])
+                    adjacent_matrix[A[i]][tuple(tmp)]='a'
+                    adjacent_check_member.append(tuple(tmp))
 
-                            tmp_birth_list=[]
+                if max_dim >= l_4: 
 
-                            for k in S_0:
-                                j=k[0]
-                                a,b=A
-                                if j not in A:
-                                    if adjacent_matrix[a][j] + adjacent_matrix[b][j]==2:
-                                        new=list(set(list(A)+[j]))                                   
-                                        row_loca=[matrix_index_dict[A]]
+                    tmp_birth_list=[]
 
-                                        new_member=[] 
-                                        for i in range(l_4):
-                                            a=list(A)
-                                            c=a.copy()
-                                            c[i]=j
-                                            c.sort()
-                                            new_member.append(tuple(c))                                    
+                    for k in S_0:
+                        j=k[0]
 
-                                        for mem in new_member:
-                                            row_loca.append(matrix_index_dict[mem])
-                                        row_loca.sort()
-                                        tmp_birth_list.append(((tuple(new),B),tuple(row_loca)))                                    
+                        if j not in A:
+                            check_value=0
 
-                            birth_list=tmp_birth_list+birth_list
-                    else:
-                        break            
- 
+                            for z in adjacent_check_member:
+                                check_value+=len(adjacent_matrix[j][z])
+
+                            if check_value==l_4: 
+                                row_loca=[matrix_index_dict[A]]
+                                new_member=[]
+                                #j 점과 연결된 a, b 점들의 엣지 만들기
+                                for i in range(l_4):
+                                    a=list(A)
+                                    c=a.copy()
+                                    c[i]=j
+                                    c.sort()
+                                    new_member.append(tuple(c))
+
+                                for mem in new_member:
+
+                                    row_loca.append(matrix_index_dict[mem])
+                                row_loca.sort()
+                                new=list(A)+[j]
+                                new.sort()
+                                tmp_birth_list.append(((tuple(new),B),tuple(row_loca)))
+
+                    birth_list=tmp_birth_list+birth_list
+            else:
+                break
+
+
+
     D=matrix_col_nonzero
     R=D.copy()
     V={}
@@ -308,10 +271,10 @@ def Caculate_Vietoris_Ripscomplex_Standardalgorithm(distmat,max_homology_dim):
                     death=col_to_value[low_R[j]]
                     if birth != death:
                         a,b=matrix_col_to_simplex[low_R[j]]
-                        globals()[f'barcode{l}'].append([(birth,death),[a],[b]])
+                        globals()[f'barcode{l}'].append([(birth,death),(a,),(b,)])
                         globals()[f'cycle{l}'].append([a,b])
                 else:
-                    globals()[f'barcode{l}'].append([(birth,'infty'),[S]])
+                    globals()[f'barcode{l}'].append([(birth,'infty'),S])
                     globals()[f'cycle{l}'].append(list(S))
             elif 1<= l <= max_homology_dim:
                 if (j in low_R):
@@ -338,7 +301,8 @@ def Caculate_Vietoris_Ripscomplex_Standardalgorithm(distmat,max_homology_dim):
                 continue
         else:
             continue
-    return barcode0, barcode1#, matrix_index,matrix_index_novalue,matrix_index_dict,matrix_col_nonzero,simplex_value,R,D,V, low_R
+    
+    return matrix_index_dict,matrix_col_nonzero,simplex_value,R,D,V, low_R
 
 
 def cycle1_index(show=None):
@@ -368,51 +332,98 @@ def cycle1_eng(show=None):
             print('C_{} : {}'.format(i,cycle))
     return Cycles
 
+def Overlapmatrix(Cycles, allnode,scale,normalized=False):
+    """
+    Cycles, allnode, nodelist, scale을 정해주면 overlapmatrix를 구해주는 코드
+    """
+    c=Cycles
+    cyc_len=[len(ci) for ci in c]
+    cyc_len_max=max(cyc_len)
+    matrix = []; 
+    for ci in c: 
+        row = []
+        for node in allnode:
+            if node in ci:
+                row.append(1)
+            else:
+                row.append(0)
+        matrix.append(row)
+    q=scale
+    mat = [];
+    for j,row in enumerate(matrix):
+        if normalized==True:
+            q=scale*cyc_len[j]/cyc_len_max         
+        new = []; leng = 0
+        for i,node in enumerate(row):
+            if node == 0:
+                if leng == 0:
+                    new.append(0)
+                else:
+                    if leng >= q:
+                        new += row[i-leng:i]+[0]
+                    else:
+                        new += [0]*leng+[0]
+                    leng = 0
+            else:
+                leng += 1
+        if leng != 0:
+            if leng >= q:
+                new += row[len(row)-leng:]
+            else:
+                new += [0]*leng
+        mat.append(new)
+    
+    return mat
 
+
+
+###정보의 계산
+def CalculateInfo(folder,songname,max_homology_dim,unit_beat_length=11520,inverse_edge_weight=True,weight_finding_path=True,vertex_weight=False):
+    info=PrepareInfo(folder,songname)
+    distmat,allnode, nodelist = analysis_from_info(info,unit_beat_length,inverse_edge_weight,weight_finding_path,vertex_weight)
+    matrix_index_dict,matrix_col_nonzero,simplex_value,R,D,V, low_R=Caculate_Vietoris_Ripscomplex_Standardalgorithm(distmat,max_homology_dim)
+    Cycles=cycle1_eng()
+    overlapmatrix=Overlapmatrix(Cycles, allnode,scale=4)
+    result={'songname':songname,'distmat':distmat,'allnode':allnode, 'nodelist':nodelist,'Cycles':Cycles,'overlapmatrix':overlapmatrix,'matrix_index_dict':matrix_index_dict,'matrix_col_nonzero':matrix_col_nonzero,'simplex_value':simplex_value,'R':R,'D':D,'V':V, 'low_R':low_R}
+    
+    return result
 
 
 ###정보의 표시
-def Show_barcode(homology_dimension,expression='index'):
+def Show_barcode(homology_dimension=None,expression='index'):
     """
     계산해낸 barcode를 보기 좋게 보여주는 코드
     """
-    if expression == 'index':
-        l=homology_dimension
-        print(f'\n {l}th homology: \n')
+    if homology_dimension==None:
+        dim_list=[0,1,2]
+    else:
+        dim_list=[homology_dimension]
+
+    if 0 in dim_list:
+        print(f'\n 0th homology: \n')
+        for i in range(len(barcode0)):
+            L=barcode0[i]
+            birth = round(L[0][0],2)
+            death = L[0][1]
+            if type(death)!=str:
+                death = round(L[0][1],2)
+            print(f'B{i+1}= [{birth},{death}) : {L[1:]}')        
+    if 1 in dim_list:
+        print(f'\n 1th homology: \n')
         for i in range(len(barcode1)):
             L=barcode1[i]
             birth = L[0][0]
             death = L[0][1]
-
-            e=''
-            for j in range(1,len(L)):
-                e+=f' + {list(L[j])}'
-            print(f'B{i+1}= [{round(birth,2)},{round(death,2)}) : {e[2:]}') 
-    if expression == 'english':
-        l=homology_dimension
-        print(f'\n {l}th homology: \n')
-        for i in range(len(barcode1)):
-            L=barcode1[i]
+            if expression == 'index':
+                Cycles=cycle1_index()
+            elif expression == 'english':
+                Cycles=cycle1_eng()
+            cycle=Cycles[i]
+            print(f'B{i+1}= [{round(birth,2)},{round(death,2)}) : {cycle}')
+    if 2 in dim_list:
+        print(f'\n 2th homology: \n')
+        for i in range(len(barcode2)):
+            L=barcode2[i]
             birth = L[0][0]
             death = L[0][1]
-
-            e=''
-            for j in range(1,len(L)):
-                tmp=[]
-                for k in L[j]:
-                    tmp.append(nodelist[k])                   
-                e+=f' + {list(tmp)}'
-            print(f'B{i+1}= [{round(birth,2)},{round(death,2)}) : {e[2:]}')  
-            
-def Show_1cycle(expression='index'):
-    """
-    1cycle을 보기 좋게 보여주는 코드
-    """
-    if expression == 'index':
-        Cycle=cycle1_index()
-        for i in range(len(Cycle)):
-            print('C_{} : {}'.format(i+1,Cycle[i]))
-    if expression == 'english':
-        Cycle=cycle1_eng()
-        for i in range(len(Cycle)):
-            print('C_{} : {}'.format(i+1,Cycle[i]))
+            print(f'B{i+1}= [{round(birth,2)},{round(death,2)}) : {L[1:]}')  
